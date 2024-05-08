b1832b63ce779c986c6066aeff78d38a00ad2e8e
pb9743 <162552506+pb9743@users.noreply.github.com>
2024-05-08 16:56:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired region
+  region = "us-east-1"
 }
 
 resource "aws_instance" "web_server" {
   ami           = "ami-07caf09b362be10b8"  # Replace with desired AMI ID
   instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_launch_profile.arn
   tags = {
     Name = "Terraform-Deployed-Webserver"
   }
