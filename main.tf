cadc09d262da05cc3d9c8710d39629e3bccbd64c
pb9743 <162552506+pb9743@users.noreply.github.com>
2024-05-08 19:08:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami           = "ami-07caf09b362be10b8"  # Replace with desired AMI ID
   instance_type = "t2.micro"
-  iam_instance_profile = aws_iam_instance_profile.ec2_launch_profile.arn
   tags = {
     Name = "Terraform-Deployed-Webserver"
   }
