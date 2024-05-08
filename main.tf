7f1036068f6551a38017be26c71ec8517d82f796
pb9743 <162552506+pb9743@users.noreply.github.com>
2024-05-08 11:39:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-07caf09b362be10b8"  # Replace with desired AMI ID
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform-Deployed-Webserver"
+  }
+}
