fb5de4f858c2322d3e0085cb045ca38d9dfeb9a6
pb9743 <162552506+pb9743@users.noreply.github.com>
2024-05-08 19:32:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web_server" {
   ami           = "ami-07caf09b362be10b8"  # Replace with desired AMI ID
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
   tags = {
     Name = "Terraform-Deployed-Webserver"
   }
