94ecbc7146ed0420f79029d96827ef7728c120a4
pb9743 <162552506+pb9743@users.noreply.github.com>
2024-05-08 14:28:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # Replace with your desired region
-  access_key_id = var.aws_access_key_id
-  secret_access_key = var.aws_secret_access_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "web_server" {
